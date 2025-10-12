1acbce8c6d0748a2fb5be4822ad7c066339dd9bf
Neeraj Deshmukh <neerajd002@gmail.com>
2025-10-13 00:38:52 +0530

    timeline update

--- ads/adScript.js
+++ ads/adScript.js
@@ -267,7 +267,7 @@ const acunx = {
     collapseAd: () => {
         adScroll.enable();
         adContainer.dataset.ad = COLLAPSED;
-        scrollAnimation.play();
+        scrollAnimation.restart();
         // acunx.banner.element.focus();
         // expandedAnimation.progress(1);
         // show(acunx.banner.element);
